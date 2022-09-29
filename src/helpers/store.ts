ef1f0ea54c8b26592c93fa09f24c15cd4c8a0634
Syed Zeeshan Akthar <akhtar.syedzeeshan@gmail.com>
2022-09-29 12:53:00 +0500

    Fixed localStorage switch for node

--- src/helpers/store.ts
+++ src/helpers/store.ts
@@ -1,23 +1,30 @@
-if (typeof localStorage === "undefined" || localStorage === null) {
-    var LocalStorage = require('node-localstorage').LocalStorage;
-    localStorage = new LocalStorage('./scratch');
-}
-
 /// State class useful for storing, getting and removing item
 export class Store {
 
+    static storage: any;
+
     /// Set item by [key] and [data]
     static setItem(key: string, data: any) {
-        localStorage.setItem(key, data);
+        this.storage.setItem(key, data);
     }
 
     /// Get item by [key]
     static getItem(key: string) {
-        return localStorage.getItem(key);
+        return this.storage.getItem(key);
     }
 
     /// Remove item by [key]
     static removeItem(key: string) {
-        localStorage.removeItem(key);
+        this.storage.removeItem(key);
     }
+}
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+    /* eslint-disable @typescript-eslint/no-var-requires */
+    /* tslint:disable no-var-requires */
+    const LocalStorage = require('node-localstorage').LocalStorage;
+    Store.storage = new LocalStorage('./scratch');
+}
+else {
+    Store.storage = localStorage;
 }

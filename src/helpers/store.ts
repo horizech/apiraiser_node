1c7b1f2bd4ae59ac22ad37d917f38ec982346413
Syed Zeeshan Akthar <akhtar.syedzeeshan@gmail.com>
2022-09-29 12:27:24 +0500

    Use node-localstorage instead of store2

--- src/helpers/store.ts
+++ src/helpers/store.ts
@@ -1,20 +1,23 @@
-import store from "store2";
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+}
 
 /// State class useful for storing, getting and removing item
 export class Store {
 
     /// Set item by [key] and [data]
     static setItem(key: string, data: any) {
-        store.set(key, data, true);
+        localStorage.setItem(key, data);
     }
 
     /// Get item by [key]
     static getItem(key: string) {
-        return store.get(key);
+        return localStorage.getItem(key);
     }
 
     /// Remove item by [key]
     static removeItem(key: string) {
-        store.remove(key);
+        localStorage.removeItem(key);
     }
 }

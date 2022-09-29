95894cc478caa9f05164f6a324d8142be25f72ad
Syed Zeeshan Akthar <akhtar.syedzeeshan@gmail.com>
2022-09-29 23:08:16 +0500

    Implemented Rest params interface

--- src/helpers/json_helper.ts
+++ src/helpers/json_helper.ts
@@ -1,23 +1,9 @@
 export class JsonHelper {
-  // static toJsonObject<T>(properties: any) {
-  //     return <TActual extends T>(value: TActual) => {
-  //         const result = {} as any;
-  //         for (const property of Object.keys(properties) as (keyof T)[]) {
-  //             const propertyPascalCase: string = property.toString().replace(/(\w)(\w*)/g,
-  //             (g0,g1,g2) => {return g1.toUpperCase() + g2.toLowerCase();});
-  //             result[propertyPascalCase] = value[property];
-  //         }
-  //         return result;
-  //     }
-  // }
-
   static toJsonObject<T>(properties: any) {
     const result = {} as any;
     for (const property of Object.keys(properties) as (keyof T)[]) {
       const propertyStr = property.toString();
       const propertyPascalCase: string = propertyStr[0].toUpperCase() + propertyStr.substring(1);
-      // const propertyPascalCase: string = property.toString().replace(/(\w)(\w*)/g,
-      // (g0,g1,g2) => {return g1.toUpperCase() + g2.toLowerCase();});
       result[propertyPascalCase] = properties[property];
     }
     return result;

4af371783f7716609b6bd92312d7a8e8a3971896
Syed Zeeshan Akthar <akhtar.syedzeeshan@gmail.com>
2022-10-22 21:58:53 +0500

    Updated createTable API

--- src/api/table.ts
+++ src/api/table.ts
@@ -5,10 +5,10 @@ import { ColumnInfo } from '../interfaces';
 export class Table {
   /// Create a new table
 
-  async create(table: string, tableType: string, columns: ColumnInfo[] = []) {
+  async create(table: string, tableCollection: string, columns: ColumnInfo[] = []) {
     const result = await Rest.Post({
-      url: `/API/CreateTable?table=${table}&tableType=${tableType}`,
-      data: columns
+      url: `/API/CreateTable?table=${table}&tableCollection=${tableCollection}`,
+      data: columns,
     });
     return result;
   }

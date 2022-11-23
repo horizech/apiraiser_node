64909f81dc957b0e3198b25dafe640e9f64707ea
Syed Zeeshan Akthar <akhtar.syedzeeshan@gmail.com>
2022-11-23 17:20:48 +0500

    Added getTemplatesList API

--- src/api/miscellaneous.ts
+++ src/api/miscellaneous.ts
@@ -2,11 +2,18 @@ import { Rest } from '../helpers';
 
 /// Miscellaneous APIs
 export class Miscellaneous {
+  /// Get list of all templates
+  async getTemplatesList() {
+    const result = await Rest.Get({ url: `/API/Apiraiser/GetTemplatesList` });
+    return result;
+  }
+
   /// Get list of all actions
   async getAllActions() {
     const result = await Rest.Get({ url: `/API/Apiraiser/GetAllActions` });
     return result;
   }
+
   /// Get Apiraiser information
   async getInfo() {
     const result = await Rest.Get({ url: `/API/Apiraiser/GetInfo` });

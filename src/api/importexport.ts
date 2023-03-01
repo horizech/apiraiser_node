2268a4234bd18b0c9e52d3a7fedc27cbb98fb89c
Syed Zeeshan Akthar <akhtar.syedzeeshan@gmail.com>
2023-03-01 19:11:18 +0500

    Added ImportExcel API

--- src/api/importexport.ts
+++ src/api/importexport.ts
@@ -2,13 +2,23 @@ import { Rest } from '../helpers';
 import { QuerySearchItem } from '../interfaces';
 
 /// Import Export APIs
-export class ImportExport {  
-  /// export excel
+export class ImportExport {
+  /// Export Excel
   async ExportExcel(table: string, conditions: QuerySearchItem[] = []) {
     const result = await Rest.Post({
       url: `/API/ImportExport/ExportExcel/${table}`,
       data: conditions,
-      responseType: 'blob'
+      responseType: 'blob',
+    });
+    return result;
+  }
+  /// Import Excel
+  async ImportExcel(table: string, file: any) {
+    const result = await Rest.Post({
+      url: `/API/ImportExport/ImportExcel/${table}`,
+      data: {
+        file,
+      },
     });
     return result;
   }

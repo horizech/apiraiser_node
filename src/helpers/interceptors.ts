a09a9659cc6593e2e31aa3ffa7c8a5da523c8497
aksamariyam8 <aqsa.mariyam97@gmail.com>
2024-03-20 14:47:16 +0500

    Created v3.0.0-rc.5 release

--- src/helpers/interceptors.ts
+++ src/helpers/interceptors.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const setupAxiosInterceptors = (onUnauthenticated: any) => {
   const onResponseSuccess = (response: any) => {
-    console.log('response success', response);
     const status = response.status;
     if (status === 403 || status === 401) {
       onUnauthenticated();
@@ -10,7 +9,6 @@ const setupAxiosInterceptors = (onUnauthenticated: any) => {
     return response;
   };
   const onResponseFail = (error: any) => {
-    console.log('response error', error);
     const status = error.status || error.response.status;
     if (status === 403 || status === 401) {
       onUnauthenticated();

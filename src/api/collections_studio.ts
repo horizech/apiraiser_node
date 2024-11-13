bdb723c8c24d259dfc24ec8b37f4c775a267d301
aksamariyam8 <aqsa.mariyam97@gmail.com>
2024-11-13 09:16:12 +0000

    Added CRUD APIs in collection studio

--- src/api/collections_studio.ts
+++ src/api/collections_studio.ts
@@ -1,6 +1,7 @@
 import { Rest } from '../helpers';
 import { AttributeInfo } from '../interfaces';
 import { version, apiraiser, app } from '../constants';
+import { CreateCollectionUsingDefinitionFileRequest } from '../interfaces/create_collection_using_definition_file_request';
 
 /// CollectionsStudio APIs
 export class CollectionsStudio {
@@ -14,16 +15,16 @@ export class CollectionsStudio {
   /// Add a new attribute
   async insertAttribute(collection: string, attributeInfo: AttributeInfo) {
     const result = await Rest.Post({
-      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/AddAttribute?collection=${collection}`,
+      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Attribute/${collection}`,
       data: attributeInfo,
     });
     return result;
   }
 
-  async modifyCollection(data: object) {
-    const result = await Rest.Put({
-      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/ModifyCollection`,
-      data: data,
+  /// Get Attributes
+  async getAttributes(collection: string) {
+    const result = await Rest.Get({
+      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Attributes/${collection}`,
     });
     return result;
   }
@@ -31,7 +32,16 @@ export class CollectionsStudio {
   /// Remove Attribute
   async deleteAttribute(collection: string, attribute: string) {
     const result = await Rest.Delete({
-      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/DeleteAttribute?collection=${collection}&attribute=${attribute}`,
+      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Attribute/${collection}/${attribute}`,
+    });
+    return result;
+  }
+
+  /// Modify Collection
+  async modifyCollection(data: object) {
+    const result = await Rest.Put({
+      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Collection`,
+      data: data,
     });
     return result;
   }
@@ -39,7 +49,7 @@ export class CollectionsStudio {
   /// Delete a collection
   async deleteCollection(collection: string) {
     const result = await Rest.Delete({
-      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/DeleteCollection?collection=${collection}`,
+      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Collection/${collection}`,
     });
     return result;
   }
@@ -48,6 +58,38 @@ export class CollectionsStudio {
   async getForeignCollections() {
     const result = await Rest.Get({
       url: `/${apiraiser}/${version}/${app}/CollectionsStudio/GetForeignCollections`,
+    });
+    return result;
+  }
+
+  /// Create a new collection
+  async createCollection(collection: string, tags: string, attributes: AttributeInfo[] = []) {
+    const result = await Rest.Post({
+      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Collection/${collection}?tags=${tags}`,
+      data: attributes,
+    });
+    return result;
+  }
+
+  /// Create a new collection using Definition File
+  async createCollectionUsingDefinitionFile(request: CreateCollectionUsingDefinitionFileRequest) {
+    const result = await Rest.Post(
+      {
+        url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Collection/CreateCollectionUsingDefinitionFile`,
+        data: request,
+      },
+      null,
+      {
+        'Content-Type': 'multipart/form-data',
+      },
+    );
+    return result;
+  }
+  /// Download collection Definition File
+  async downloadCollectionDefinitionFile(collection: string) {
+    const result = await Rest.Get({
+      url: `/${apiraiser}/${version}/${app}/CollectionsStudio/Collection/DownloadCollectionDefinitionFile/${collection}`,
+      responseType: 'blob',
     });
     return result;
   }

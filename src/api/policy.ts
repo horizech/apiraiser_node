9b69ddc92e31fc80252797e6db7a3422c6cd6dab
aksamariyam8 <aqsa.mariyam97@gmail.com>
2024-11-13 09:56:37 +0000

    Added policy editor CRUD

--- src/api/policy.ts
+++ src/api/policy.ts
@@ -11,4 +11,22 @@ export class PolicyEditor {
     const result = await Rest.Get({ url });
     return result;
   }
+
+  /// Insert a new record
+  async insert(collection: string, data: object) {
+    const result = await Rest.Post({ url: `/${apiraiser}/${version}/${app}/PolicyEditor/${collection}`, data });
+    return result;
+  }
+
+  /// Update record by id
+  async update(collection: string, id: string, data: object) {
+    const result = await Rest.Put({ url: `/${apiraiser}/${version}/${app}/PolicyEditor/${collection}/${id}`, data });
+    return result;
+  }
+
+  /// Delete record by id
+  async delete(collection: string, id: string) {
+    const result = await Rest.Delete({ url: `/${apiraiser}/${version}/${app}/PolicyEditor/${collection}/${id}` });
+    return result;
+  }
 }

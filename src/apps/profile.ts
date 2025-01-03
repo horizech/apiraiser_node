66c9134c4da78ddf5bf251dfc4fe25dbce3579e6
aksamariyam8 <aqsa.mariyam97@gmail.com>
2025-01-03 17:33:02 +0500

    Added new APIs in profile app

--- src/apps/profile.ts
+++ src/apps/profile.ts
@@ -3,7 +3,7 @@ import { version, apiraiser, app } from '../constants';
 
 /// Profile App APIs
 export class ProfileApp {
-  /// Get records
+  /// Get User Detail
   async getUserDetail() {
     const url = `/${apiraiser}/${version}/${app}/Profile`;
     const result = await Rest.Get({
@@ -11,6 +11,8 @@ export class ProfileApp {
     });
     return result;
   }
+
+  /// Get user media
   async getUserMedia() {
     const url = `/${apiraiser}/${version}/${app}/Profile/GetMedia`;
     const result = await Rest.Get({
@@ -18,14 +20,8 @@ export class ProfileApp {
     });
     return result;
   }
-  async updateProfile(data: object) {
-    const url = `/${apiraiser}/${version}/${app}/Profile/UpdateProfile`;
-    const result = await Rest.Put({
-      url,
-      data,
-    });
-    return result;
-  }
+
+  // Update Username
   async updateUsername(data: object) {
     const url = `/${apiraiser}/${version}/${app}/Profile/UpdateUsername`;
     const result = await Rest.Put({
@@ -34,6 +30,8 @@ export class ProfileApp {
     });
     return result;
   }
+
+  // Update Profile Picture
   async updateProfilePicture(data: object) {
     const url = `/${apiraiser}/${version}/${app}/Profile/UpdateProfilePicture`;
     const result = await Rest.Put({
@@ -42,6 +40,8 @@ export class ProfileApp {
     });
     return result;
   }
+
+  // Update Profile Cover
   async updateProfileCover(data: object) {
     const url = `/${apiraiser}/${version}/${app}/Profile/UpdateProfileCover`;
     const result = await Rest.Put({
@@ -50,16 +50,41 @@ export class ProfileApp {
     });
     return result;
   }
+
+  // Change Email
   async changeEmail(email: string) {
-      const result = await Rest.Post({
-        url: `/${apiraiser}/${version}/${app}/Profile/ChangeEmail`,
-        data: email,
-      });
-      return result;
-    }
+    const result = await Rest.Post({
+      url: `/${apiraiser}/${version}/${app}/Profile/ChangeEmail`,
+      data: email,
+    });
+    return result;
+  }
+
+  // Change Password
   async changePassword(id: string, password: string, currentPassword: string) {
-      const url = `/${apiraiser}/${version}/${app}/Profile/ChangePassword`;
-      const result = await Rest.Post({ url, data: {CurrentPassword: currentPassword, Password: password, Id: id } });
-      return result;
-    }
+    const url = `/${apiraiser}/${version}/${app}/Profile/ChangePassword`;
+    const result = await Rest.Post({ url, data: { CurrentPassword: currentPassword, Password: password, Id: id } });
+    return result;
+  }
+
+  // Get User Profile
+  async getUserProfile() {
+    const url = `/${apiraiser}/${version}/${app}/Profile/UserProfile`;
+    const result = await Rest.Get({ url });
+    return result;
+  }
+
+  // Insert User Profile
+  async insertUserProfile(data: object) {
+    const url = `/${apiraiser}/${version}/${app}/Profile/UserProfile`;
+    const result = await Rest.Post({ url, data });
+    return result;
+  }
+
+  // Update User Profile
+  async updateUserProfile(data: object) {
+    const url = `/${apiraiser}/${version}/${app}/Profile/UserProfile`;
+    const result = await Rest.Put({ url, data });
+    return result;
+  }
 }

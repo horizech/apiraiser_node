17fed883ddfde6758bab72ef6fea7bae4c6cc4d9
alihassan1129 <ahassan@horizech.com>
2025-01-03 15:14:32 +0500

    Update ProfileApp APIs

--- src/apps/profile.ts
+++ src/apps/profile.ts
@@ -34,4 +34,32 @@ export class ProfileApp {
     });
     return result;
   }
+  async updateProfilePicture(data: object) {
+    const url = `/${apiraiser}/${version}/${app}/Profile/UpdateProfilePicture`;
+    const result = await Rest.Put({
+      url,
+      data,
+    });
+    return result;
+  }
+  async updateProfileCover(data: object) {
+    const url = `/${apiraiser}/${version}/${app}/Profile/UpdateProfileCover`;
+    const result = await Rest.Put({
+      url,
+      data,
+    });
+    return result;
+  }
+  async changeEmail(email: string) {
+      const result = await Rest.Post({
+        url: `/${apiraiser}/${version}/${app}/Profile/ChangeEmail`,
+        data: email,
+      });
+      return result;
+    }
+  async changePassword(id: string, password: string, currentPassword: string) {
+      const url = `/${apiraiser}/${version}/${app}/Profile/ChangePassword`;
+      const result = await Rest.Post({ url, data: {CurrentPassword: currentPassword, Password: password, Id: id } });
+      return result;
+    }
 }
